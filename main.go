e399838b979da811ec2428e2954f12ecfd0b499a
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-07-26 22:38:44 +0000

    refactor: Cleanup MVP implementation

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"sync"
 
 	"github.com/alessio/shellescape"
@@ -22,7 +21,7 @@ const (
 )
 
 var (
-	goToolDistListArgs = []string{"tool", "dist", "list", "--json"}
+	listSupportedTargets = []string{"tool", "dist", "list", "--json"}
 )
 
 type Platform struct {
@@ -30,7 +29,7 @@ type Platform struct {
 	GoArch string `json:"GOARCH"`
 }
 
-func getSuffixForGoOSAndGOArch(goOS, goArch string) string {
+func getBinIdentifier(goOS, goArch string) string {
 	if goOS == "windows" {
 		return ".exe"
 	}
@@ -42,14 +41,14 @@ func getSuffixForGoOSAndGOArch(goOS, goArch string) string {
 	return ""
 }
 
-func getUnameConventionForGoArch(goArch string) string {
+func getArchIdentifier(goArch string) string {
 	switch goArch {
 	case "386":
 		return "i686"
 	case "amd64":
 		return "x86_64"
 	case "arm":
-		return "armv7l" // Best guess, could also be `armv6l` etc. depending on `GOARCH`
+		return "armv7l" // Best effort, could also be `armv6l` etc. depending on `GOARCH`
 	case "arm64":
 		return "aarch64"
 	default:
@@ -62,7 +61,7 @@ func main() {
 	pflag.Usage = func() {
 		fmt.Printf(`Build for all Go-supported platforms by default, disable those which you don't want.
 
-Example usage: %s -n myexample -j $(nproc) -d out -b '(android/arm$|ios/*|openbsd/mips64)' main.go
+Example usage: %s -b mybin -x '(android/arm$|ios/*|openbsd/mips64)' -j $(nproc) main.go
 
 See https://github.com/pojntfx/bagop for more information.
 
@@ -73,12 +72,12 @@ Usage: %s [OPTION...] <INPUT>
 	}
 
 	// Parse flags
-	nameFlag := pflag.StringP("name", "n", "example", "Prefix of resulting binary")
-	distFlag := pflag.StringP("dist", "d", "out", "Directory to build to")
-	blockFlag := pflag.StringP("block", "b", "", "Regex of platforms not to build for, i.e. (windows/386|linux/mips64)")
-	extraArgs := pflag.StringP("extraArgs", "e", "", "Extra arguments to pass to the Go compiler")
-	jobFlag := pflag.Int64P("jobs", "j", 1, "Maximum amount of parallel jobs")
-	goarchConventions := pflag.BoolP("goarchConventions", "g", false, "Use GOARCH conventions in resulting file names (i.e. amd64). If not specified, uname's conventions (i.e. x86_64) will be used.")
+	binFlag := pflag.StringP("bin", "b", "mybin", "Prefix of resulting binary")
+	distFlag := pflag.StringP("dist", "d", "out", "Directory build into")
+	excludeFlag := pflag.StringP("exclude", "x", "", "Regex of platforms not to build for, i.e. (windows/386|linux/mips64)")
+	extraArgs := pflag.StringP("extra-args", "e", "", "Extra arguments to pass to the Go compiler")
+	jobsFlag := pflag.Int64P("jobs", "j", 1, "Maximum amount of parallel jobs")
+	goismsFlag := pflag.BoolP("goisms", "g", false, "Use Go's conventions (i.e. amd64) instead of uname's conventions (i.e. x86_64)")
 
 	pflag.Parse()
 
@@ -96,31 +95,33 @@ Usage: %s [OPTION...] <INPUT>
 	}
 
 	// Interpret arguments
-	input := strings.Join(pflag.Args(), " ")
+	input := pflag.Args()[0]
 
-	// Construct the platform query command
-	cmd := exec.Command(goCmd, goToolDistListArgs...)
+	// Construct platform query command
+	queryCmd := exec.Command(goCmd, listSupportedTargets...)
 
 	// Capture stdout and stderr
-	var outb, errb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
+	var queryStdout, queryStderr bytes.Buffer
+	queryCmd.Stdout = &queryStdout
+	queryCmd.Stderr = &queryStderr
 
 	// Get supported platforms
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Could not query supported platforms: err=%v, stdout=%v, stderr=%v", err, outb.String(), errb.String())
+	if err := queryCmd.Run(); err != nil {
+		log.Fatalf("could not query supported platforms: err=%v, stdout=%v, stderr=%v", err, queryStdout.String(), queryStderr.String())
 	}
 
+	// Parse platforms
 	parsedPlatforms := []Platform{}
-	if err := json.Unmarshal(outb.Bytes(), &parsedPlatforms); err != nil { // It is safe to read outb.Bytes() here as we would only read it in `cmd.Run()` above if we were to error, in which case we would exit first
-		log.Fatal("Could not parse supported platforms:", err)
+	if err := json.Unmarshal(queryStdout.Bytes(), &parsedPlatforms); err != nil { // It is safe to read outb.Bytes() here as we would only read it in `cmd.Run()` above if we were to error, in which case we would exit first
+		log.Fatal("could not parse supported platforms:", err)
 	}
 
+	// Limits the max. amount of concurrent builds
 	// See https://play.golang.org/p/othihEtsOBZ
 	var wg = sync.WaitGroup{}
-	guard := make(chan struct{}, *jobFlag)
+	guard := make(chan struct{}, *jobsFlag)
 
-	for _, oplatform := range parsedPlatforms {
+	for _, lplatform := range parsedPlatforms {
 		guard <- struct{}{}
 		wg.Add(1)
 
@@ -131,69 +132,64 @@ Usage: %s [OPTION...] <INPUT>
 				<-guard
 			}()
 
-			// Process suffixes
-			suffix := ""
+			// Construct the filename
+			output := filepath.Join(*distFlag, *binFlag+"."+platform.GoOS+"-")
 
-			// Add `.exe` suffix on Windows
-			if platform.GoOS == "windows" {
-				suffix = ".exe"
+			// Add the arch identifier
+			archIdentifier := getArchIdentifier(platform.GoArch)
+			if *goismsFlag {
+				archIdentifier = platform.GoArch
 			}
+			output += archIdentifier
 
-			// Add `.wasm` suffix on WASM
-			if platform.GoOS == "js" && platform.GoArch == "wasm" {
-				suffix = ".wasm"
-			}
+			// Add the binary identifier
+			output += getBinIdentifier(platform.GoOS, platform.GoArch)
 
-			// Construct the binary name
-			resultingName := filepath.Join(*distFlag, *nameFlag+"."+platform.GoOS+"-")
-			resultingIdentifier := getUnameConventionForGoArch(platform.GoArch) + suffix
-			if *goarchConventions {
-				resultingIdentifier = platform.GoArch + suffix
-			}
-			resultingName += resultingIdentifier
-			resultingName += getSuffixForGoOSAndGOArch(platform.GoOS, platform.GoArch)
-
-			// Check if current platform matches the regex
-			matched, err := regexp.MatchString(*blockFlag, platform.GoOS+"/"+platform.GoArch)
+			// Check if current platform should be skipped
+			skip, err := regexp.MatchString(*excludeFlag, platform.GoOS+"/"+platform.GoArch)
 			if err != nil {
-				log.Fatal("Could not match check if platform should be blocked based on regex:", err)
+				log.Fatal("could not match check if platform should be blocked based on regex:", err)
 			}
 
-			// Skip the platform if it matches the block regex
-			if matched {
+			// Skip the platform if it matches the exclude regex
+			if skip {
 				log.Printf("%v %v/%v (platform matched the provided regex)", color.New(color.FgYellow).SprintFunc()("skipping"), color.New(color.FgCyan).SprintFunc()(platform.GoOS), color.New(color.FgMagenta).SprintFunc()(platform.GoArch))
 
 				return
 			}
 
-			// Continue if it does not match the regex
-			log.Printf("%v %v/%v (%v)", color.New(color.FgGreen).SprintFunc()("building"), color.New(color.FgCyan).SprintFunc()(platform.GoOS), color.New(color.FgMagenta).SprintFunc()(platform.GoArch), resultingName)
+			// Continue if platform is enabled
+			log.Printf("%v %v/%v (%v)", color.New(color.FgGreen).SprintFunc()("building"), color.New(color.FgCyan).SprintFunc()(platform.GoOS), color.New(color.FgMagenta).SprintFunc()(platform.GoArch), output)
 
-			// Construct the build command
-			args := append(append([]string{"build", "-o"}, []string{shellescape.Quote(resultingName)}...), shellescape.Quote(input))
+			// Construct build args
+			buildArgs := append([]string{"build", "-o"}, []string{shellescape.Quote(output)}...)
 
 			// Add the extra args if they are defined
 			if *extraArgs != "" {
-				args = append([]string{*extraArgs}, args...)
+				buildArgs = append(buildArgs, *extraArgs)
 			}
 
-			cmd := exec.Command(goCmd, args...)
+			// Add the input
+			buildArgs = append(buildArgs, shellescape.Quote(input))
+
+			// Construct build command
+			buildCmd := exec.Command(goCmd, buildArgs...)
 
 			// Set `GOOS` and `GOARCH` env vars
-			cmd.Env = os.Environ()
-			cmd.Env = append(cmd.Env, "GOOS="+shellescape.Quote(platform.GoOS), "GOARCH="+shellescape.Quote(platform.GoArch))
+			buildCmd.Env = os.Environ()
+			buildCmd.Env = append(buildCmd.Env, "GOOS="+shellescape.Quote(platform.GoOS), "GOARCH="+shellescape.Quote(platform.GoArch))
 
 			// Capture stdout and stderr
-			var outb, errb bytes.Buffer
-			cmd.Stdout = &outb
-			cmd.Stderr = &errb
+			var buildStdout, buildStderr bytes.Buffer
+			buildCmd.Stdout = &buildStdout
+			buildCmd.Stderr = &buildStderr
 
 			// Start the build
-			if err := cmd.Run(); err != nil {
-				log.Fatalf("Could not build for platform %v/%v: err=%v, stdout=%v, stderr=%v", platform.GoOS, platform.GoArch, err, outb.String(), errb.String())
+			if err := buildCmd.Run(); err != nil {
+				log.Fatalf("could not build for platform %v/%v: err=%v, stdout=%v, stderr=%v", platform.GoOS, platform.GoArch, err, buildStdout.String(), buildStderr.String())
 			}
 
-		}(oplatform)
+		}(lplatform)
 	}
 
 	wg.Wait()
